1b5a8eb54b58f74b265ad98c90f1e0cc9851ccd9
Jono Mingard <reason.koan@gmail.com>
2016-04-09 18:18:25 +1200

    Allow the player to dash, increasing speed and push power

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 [RequireComponent(typeof (CharacterController))]
@@ -6,26 +7,61 @@ public class PlayerMovement : MonoBehaviour {
 	public float speed = 3;
 	public float pushStrength = 5;
 
+	public float dashSpeedMultiplier = 2;
+	public float dashPushMultiplier = 10;
+
+	public Text dashDisplay;
+
 	private CharacterController characterController;
+
+	private const int MAX_DASH = 50;
+	private int dashRemaining = MAX_DASH;
+	private bool dashRecharging = false;
 
 	void Start() {
 		characterController = GetComponent<CharacterController>();
 	}
 
 	void FixedUpdate() {
-		var forwardMovement = transform.TransformDirection(Vector3.forward) * verticalSpeed();
-		var sidewaysMovement = transform.TransformDirection(Vector3.right) * horizontalSpeed();
+		var movement =
+			transform.TransformDirection(Vector3.forward) * verticalSpeed() +
+			transform.TransformDirection(Vector3.right) * horizontalSpeed();
+		
+		if (isDashing()) {
+			movement *= dashSpeedMultiplier;
 
-		characterController.SimpleMove(forwardMovement + sidewaysMovement);
+			dashRecharging = false;
+			dashRemaining--;
+		} else if (!dashRecharging && (dashRemaining == 0 || (!isDashing() && dashRemaining < MAX_DASH))) {
+			dashRecharging = true;
+		} else if (dashRecharging && dashRemaining < MAX_DASH) {
+			dashRemaining++;
+
+			if (dashRemaining == MAX_DASH) {
+				dashRecharging = false;
+			}
+		}
+
+		characterController.SimpleMove(movement);
+	}
+
+	void Update() {
+		dashDisplay.text = "Dash: " + dashRemaining.ToString();
 	}
 
 	void OnControllerColliderHit(ControllerColliderHit hit) {
 		var other = hit.gameObject;
 
 		if (other.CompareTag("Pickup") && hit.rigidbody) {
-			var pushDirection = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+			var push = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z) * pushStrength;
 
-			other.GetComponent<Rigidbody>().AddForce(pushDirection * pushStrength);
+			if (isDashing()) {
+				// Push the object harder and slightly upwards
+				push.y = 5;
+				push *= dashPushMultiplier;
+			}
+
+			other.GetComponent<Rigidbody>().AddForce(push);
 		}
 	}
 
@@ -35,5 +71,9 @@ public class PlayerMovement : MonoBehaviour {
 
 	private float verticalSpeed() {
 		return speed * Input.GetAxis("Vertical");
+	}
+
+	private bool isDashing() {
+		return !dashRecharging && Input.GetKey(KeyCode.Space) && dashRemaining > 0;
 	}
 }
