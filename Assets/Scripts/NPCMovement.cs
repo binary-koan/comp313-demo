c3d6417e5d2a83f7732d6e9b7040cea09aef3216
Jono Mingard <reason.koan@gmail.com>
2016-04-12 17:56:45 +1200

    Make NPC follow the player if they come close

---
+++ Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NPCMovement : MonoBehaviour {
+    enum State {
+        MOVING, FOLLOWING_PLAYER, PAUSED
+    }
+
+    public GameObject player;
+    public Transform[] movePoints;
+	public float speed = 5;
+
+	private const float STOPPING_DISTANCE_THRESHOLD = 0.2f;
+    private const float PAUSE_SECONDS = 2;
+    private const float FOLLOW_PLAYER_DISTANCE = 5;
+
+    private State state = State.MOVING;
+    private float pausedTime = 0;
+
+	private Transform targetMovePoint;
+	private NavMeshAgent navMeshAgent;
+    private Animator modelAnimator;
+
+	void Start() {
+		navMeshAgent = GetComponent<NavMeshAgent>();
+        modelAnimator = GetComponentInChildren<Animator>();
+        modelAnimator.SetFloat("MovementSpeed", speed);
+		setMovePoint(0);
+	}
+
+	void Update() {
+        switch (state) {
+        case State.MOVING:
+            moveOnPath();
+            break;
+        case State.FOLLOWING_PLAYER:
+            moveTowardsPlayer();
+            break;
+        case State.PAUSED:
+            maybeResume();
+            break;
+        }
+	}
+
+	void OnCollisionEnter(Collision hit) {
+		if (hit.gameObject.CompareTag("Bouncer") || hit.gameObject.CompareTag("Player")) {
+            setState(State.PAUSED);
+		}
+	}
+
+    private void setState(State newState) {
+        state = newState;
+
+        switch (state) {
+        case State.MOVING:
+            navMeshAgent.Resume();
+            setNextMovePoint();
+            break;
+
+        case State.FOLLOWING_PLAYER:
+            navMeshAgent.Resume();
+            break;
+
+        case State.PAUSED:
+            pausedTime = 0;
+
+            navMeshAgent.Stop();
+            modelAnimator.SetBool("IsMoving", false);
+            break;
+        }
+    }
+
+    private void moveOnPath() {
+        if (distanceToPlayer() < FOLLOW_PLAYER_DISTANCE) {
+            setState(State.FOLLOWING_PLAYER);
+        }
+        else if (atTargetMovePoint()) {
+            setNextMovePoint();
+        }
+    }
+
+    private void moveTowardsPlayer() {
+        if (distanceToPlayer() > FOLLOW_PLAYER_DISTANCE) {
+            setState(State.MOVING);
+        }
+        else {
+            navMeshAgent.destination = player.transform.position;
+        }
+    }
+
+    private void maybeResume() {
+        pausedTime += Time.deltaTime;
+
+        if (pausedTime >= PAUSE_SECONDS) {
+            setState(State.MOVING);
+        }
+    }
+
+	private bool atTargetMovePoint() {
+		//TODO: Is there a better way to test this?
+		var xDistance = Math.Abs(transform.position.x - targetMovePoint.position.x);
+		var zDistance = Math.Abs(transform.position.z - targetMovePoint.position.z);
+		var threshold = navMeshAgent.stoppingDistance + STOPPING_DISTANCE_THRESHOLD;
+
+		return xDistance <= threshold && zDistance <= threshold;
+	}
+
+	private void setNextMovePoint() {
+		var movePointIndex = Array.IndexOf(movePoints, targetMovePoint);
+		var atLastMovePoint = (movePointIndex == movePoints.Length - 1);
+
+		setMovePoint(atLastMovePoint ? 0 : movePointIndex + 1);
+	}
+
+	private void setMovePoint(int index) {
+		targetMovePoint = movePoints[index];
+
+        navMeshAgent.destination = targetMovePoint.position;
+        modelAnimator.SetBool("IsMoving", true);
+    }
+
+    private float distanceToPlayer() {
+        return (player.transform.position - transform.position).magnitude;
+    }
+}
