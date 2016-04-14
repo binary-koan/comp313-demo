a446571729531fc00f6b2632b7d4050a397e05be
Jono Mingard <reason.koan@gmail.com>
2016-04-15 11:26:43 +1200

    Display current inventory in a Panel

--- Assets/PlayerPickup.cs
+++ Assets/PlayerPickup.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class PlayerPickup : MonoBehaviour {
 	public int grabDistance = 3;
+	public GameObject inventoryPanel;
+	public GameObject inventoryItemDisplay;
 
-	private List<GameObject> inventory = new List<GameObject>();
+	private class InventoryItem {
+		public GameObject item { get; private set; }
+		public GameObject hudItem { get; private set; }
+
+		public InventoryItem(GameObject item, GameObject hudItem) {
+			this.item = item;
+			this.hudItem = hudItem;
+		}
+	}
+
+	private List<InventoryItem> inventory = new List<InventoryItem>();
 
 	void Update () {
 		var clickedObject = findClickedObject();
 
 		if (clickedObject && canPickUp(clickedObject) && isBeside(clickedObject)) {
-			inventory.Add(clickedObject);
-			clickedObject.SetActive(false);
+			addToInventory(clickedObject);
 		}
 	}
 
@@ -32,7 +44,17 @@ public class PlayerPickup : MonoBehaviour {
 		return item.CompareTag("Bouncer");
 	}
 
-	private bool isBeside(GameObject other) {
-		return (other.transform.position - transform.position).magnitude <= grabDistance;
+	private bool isBeside(GameObject item) {
+		return (item.transform.position - transform.position).magnitude <= grabDistance;
+	}
+
+	private void addToInventory(GameObject item) {
+		item.SetActive(false);
+
+		var hudItem = (GameObject)Instantiate(inventoryItemDisplay, new Vector3(-150, -50 - 70 * inventory.Count, 0), Quaternion.identity);
+		hudItem.SetActive(true);
+		hudItem.transform.SetParent(inventoryPanel.transform, false);
+
+		inventory.Add(new InventoryItem(item, hudItem));
 	}
 }
