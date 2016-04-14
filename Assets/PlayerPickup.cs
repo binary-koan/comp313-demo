f4f4bc84ea41462bb9a0150acf1b04325b3e5cb1
Jono Mingard <reason.koan@gmail.com>
2016-04-15 10:10:04 +1200

    Allow picking up objects into an invisible inventory

---
+++ Assets/PlayerPickup.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlayerPickup : MonoBehaviour {
+	public int grabDistance = 3;
+
+	private List<GameObject> inventory = new List<GameObject>();
+
+	void Update () {
+		var clickedObject = findClickedObject();
+
+		if (clickedObject && canPickUp(clickedObject) && isBeside(clickedObject)) {
+			inventory.Add(clickedObject);
+			clickedObject.SetActive(false);
+		}
+	}
+
+	private GameObject findClickedObject() {
+		if (Input.GetMouseButtonDown(0)) {
+			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, 100)) {
+				return hit.collider.gameObject;
+			}
+		}
+
+		return null;
+	}
+
+	private bool canPickUp(GameObject item) {
+		return item.CompareTag("Bouncer");
+	}
+
+	private bool isBeside(GameObject other) {
+		return (other.transform.position - transform.position).magnitude <= grabDistance;
+	}
+}
