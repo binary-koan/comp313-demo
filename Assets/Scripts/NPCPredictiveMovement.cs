c2c03c90a857be277546aea2f9ff5bedc1133297
Jono Mingard <reason.koan@gmail.com>
2016-04-17 15:12:53 +1200

    Add second NPC to collaborate with the first

---
+++ Assets/Scripts/NPCPredictiveMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NPCPredictiveMovement : MonoBehaviour {
+    public Transform partnerTransform;
+    public Transform playerTransform;
+
+    private NavMeshAgent navMeshAgent;
+    private Animator animator;
+    
+    void Start() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+
+        animator.SetBool("IsMoving", true);
+        animator.SetFloat("MovementSpeed", navMeshAgent.speed * 1.5f);
+    }
+	
+	void Update() {
+        // The idea with this positioning is that this NPC will try to get on the opposite side of the
+        // player to its partner - as the partner moves closer to the player the NPC will move closer
+        // on the other side so that they trap the player with a pincer movement
+        var targetOffset = -(partnerTransform.position - playerTransform.position);
+
+        navMeshAgent.SetDestination(playerTransform.position + targetOffset);
+	}
+}
